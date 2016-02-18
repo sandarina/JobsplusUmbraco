fdc9d65cb4dca67cea99f09a261bac701fed512e
Darina Kourilova <=>
2016-02-18 09:33:59 +0100

    napojeni zajemce o praci na UP

---
+++ JobsplusUmbraco/App_Plugins/JobsplusGrants/EmployDepartmentEditor/editor.controller.js
@@ -0,0 +1,24 @@
+﻿angular.module("umbraco")
+    .controller("JobsplusGrants.EmployDepartmentController",
+    function ($scope, grantsResource) {
+
+        $scope.loaded = false;
+        $scope.list = [];
+
+        grantsResource.getEmployDepartments().then(function (response) {
+            //$scope.list = response.data;
+            angular.forEach(response.data, function (value, key) {
+                var regionName = "";
+                grantsResource.getRegionById(value.RegionId).then(function (response_region) {
+                    regionName = response_region.data.Name;
+                    $scope.list.push({
+                        Id: value.Id.toString(),
+                        Name: value.Name,
+                        RegionName: regionName
+                    });
+                });
+            });
+            $scope.loaded = true;
+        });
+            
+    });
